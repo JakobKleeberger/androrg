24b8e04a3fb9598bd0fed7c44201fbd32cf35a43
JakobKleeberger <kleeberger.jakob@aol.com>
2025-05-27 21:36:39 +0200

    init commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix-flake-based Java development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
+      };
+      buildToolsVersion = "34.0.0";
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [ buildToolsVersion "28.0.3" ];
+        platformVersions = [ "34" "28" ];
+        abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+      };
+      androidSdk = androidComposition.androidsdk;
+    in
+    {
+      devShell =
+        with pkgs; mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          buildInputs = [
+            flutter
+            androidSdk # The customized SDK that we've made above
+            jdk17
+	    libsForQt5.kdialog
+          ];
+        };
+    });
+}
